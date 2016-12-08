00252ef3c9e4a714cb9874df2007cea2ce67a9a0
dronzer1010 <sravik1010@gmail.com>
2016-12-08 08:15:01 +0530

    Create ticket

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,10 @@ var router  = express.Router();
 
 router.use('/api' ,require('./api'));
 
+router.get('/ticket' , function(req , res){
+	res.sendFile('ticket.html',{ root: __base + 'client' });
+});
+
 router.get('*' , function(req , res){
 	res.render('index.html');
 });
