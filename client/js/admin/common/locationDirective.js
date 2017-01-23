81f88aeaa7c8cdde28e8e87140f552a05c022f85
dronzer1010 <sravik1010@gmail.com>
2017-01-23 10:47:22 +0530

    Location api fix

--- client/js/admin/common/locationDirective.js
+++ client/js/admin/common/locationDirective.js
@@ -3,18 +3,22 @@ $(function(){
 
 	angular.module('apolloApp')
 			.directive('locations' ,['locationMasterService' ,function(locationMasterService){
-				var space=" ";
+				var space="";
 				return {
 			      restrict: 'E',
 			      replace:true,
-			      template: "<select class='form-control' data-ng-options='location as location.name for location in locations track by location._id'></select>",
+			      template: '<select class="form-control" data-ng-options="location as location.name for location in locations track by location._id"></select>',
 			      require: 'ngModel',
 			      link: function(scope, element, attrs, ngModelCtrl) {
 			        
 			        locationMasterService.fetchAllLocations()
 			        					.then(function(locations) {
-			        
+			          
 			          scope.locations = locations.data;
+								scope.locations.forEach(function(item,index){
+									item.name = item.name+' , '+item.division;
+									//console.log(item);
+								});
 			        });
 			      }
 			    };
