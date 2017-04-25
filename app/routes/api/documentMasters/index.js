dd0560c5c386f2d870856c0259fad51f7a6b62cd
dronzer1010 <sravik1010@gmail.com>
2017-04-25 14:41:09 +0530

    Document Search

--- app/routes/api/documentMasters/index.js
+++ app/routes/api/documentMasters/index.js
@@ -17,8 +17,8 @@ router.post('/search' , function(req,res){
 	if(req.body.query){
 		var q=req.body.query;
 		var query = new RegExp(q, "i");
-		Document.find({$text: {$search: req.body.query} , approved : true},{ score : { $meta: "textScore" } })
-				.sort({ score : { $meta: 'textScore' } })
+		Document.find({documentName : query })
+				
 				.exec(function(err , docs){
 			if(!err){
 				res.status(200).send({
@@ -52,6 +52,25 @@ router.post('/approve/:id' , function(req,res){
 			additionalDetails : req.body.docAdditionalDetail ,
 			approvalBy : req.body.approvedBy,
             approvalDoneBy :req.body.approvalDoneBy
+		}
+	},function(err ,data){
+		if(!err){
+                console.log(data);
+				res.status(200).json({success : true , data : data});
+			}else{
+				res.status(500).json({success : false , msg : err});
+			}
+	});
+});
+
+
+router.put('/changename/:id' , function(req,res){
+
+    console.log(req.body);
+   
+	Document.update({_id : req.params.id} , {
+		$set : {
+            documentName:req.body.newName
 		}
 	},function(err ,data){
 		if(!err){
